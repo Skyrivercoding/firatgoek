93ff0d8ec83c66f2282193a714f62e0a45900ef2
Firat <48656993+Skyrivercoding@users.noreply.github.com>
2024-07-11 23:43:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-var duration = 15 * 1000;
-var animationEnd = Date.now() + duration;
-var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
 function randomInRange(min, max) {
   return Math.random() * (max - min) + min;
 }
 
 function onclickFireworkbtn() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
   var interval = setInterval(function () {
     var timeLeft = animationEnd - Date.now();
 
